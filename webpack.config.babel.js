4cc1537247e7057f1703e6299d3839523597ef9e
James Wright <jcwright04@gmail.com>
2016-08-27 21:24:44 -0700

    fix(webpack config babel): refactor webpack babel config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,27 +1,19 @@
-import {join} from 'path';
+import {join} from 'path'
 
-const include = join(__dirname, 'src');
+const include = join(__dirname, 'src')
 
 export default {
-    entry   : './src/genrandom',
-    output  : {
-        path          : join(__dirname, 'dist'),
-        libraryTarget : 'umd',
-        library       : 'genrandom'
+    entry: './src/genrandom',
+    output: {
+        path: join(__dirname, 'dist'),
+        libraryTarget: 'umd',
+        library: 'genrandom',
     },
-    devtool : 'source-map',
-    module  : {
-        loaders : [
-            {
-                test   : /\.js$/,
-                loader : 'babel',
-                include
-            },
-            {
-                test   : /\.json$/,
-                loader : 'json',
-                include
-            }
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {test: /\.js$/, loader: 'babel', include},
+            {test: /\.json$/, 'loader': 'json', include},
         ]
     }
 }
